9163c2a11c128721af8a7ebab35754b1e8c84c57
liziane <lizianecysne1@gmail.com>
2023-07-25 14:26:49 +0000

    adiciona o desafio 1079

---
+++ beecrownd_1079.rb
@@ -0,0 +1,26 @@
+# Leia 1 valor inteiro N, que representa o número de casos de teste que vem a seguir. Cada caso de teste consiste de 3 valores reais, cada um deles com uma casa decimal. Apresente a média ponderada para cada um destes conjuntos de 3 valores, sendo que o primeiro valor tem peso 2, o segundo valor tem peso 3 e o terceiro valor tem peso 5.
+
+# Entrada
+# O arquivo de entrada contém um valor inteiro N na primeira linha. Cada N linha a seguir contém um caso de teste com três valores com uma casa decimal cada valor.
+
+# Saída
+# Para cada caso de teste, imprima a média ponderada dos 3 valores, conforme exemplo abaixo.
+
+n = gets.chomp.to_i
+pesos = [2,3,5]
+
+
+n.times do
+  input = gets.chomp
+  array = input.split.map(&:to_f)
+  
+  soma_produtos = 0
+  soma_pesos = 0
+  
+  array.each_with_index do |n, index|
+    soma_produtos += n * pesos[index]
+    soma_pesos += pesos[index]
+  end
+    media_ponderada = soma_produtos / soma_pesos   
+  puts media_ponderada.round(1)
+end
